5eb5376e836cb7e8231909133984b1a26a681b5f
xericue <106630482+xericue@users.noreply.github.com>
2026-03-10 19:02:44 +0000

    listening, connecting, setsocketopt

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 you'll need to include quite a few libraries in order to start programming with actual sockets and servers. the goal here is to create a `client-server` model, so, naturally, we'll create a `client` and a `server` - `client.cpp` and `server.cpp`
 
-the `client` reaches out to the `server`, and the `server` listens to that outreach.
+the base idea is that the `server` creates a `socket` and binds it to a port (say port 67). a `client` will then try to connect to port 67 as the server listens to that connection. **the `client` reaches out to the `server`, and the `server` listens to that outreach.**
--- server.cpp
+++ server.cpp
@@ -9,13 +9,22 @@ int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     // error handling
     if (fd == 0) {
-        printf("socket returned %d - error\n", fd);
+        std::cout << "socket returned " << fd << " - error\n";
         abort();
     }
 
     // next steps: bind and listen
     // right now, our socket is TCP but typeless. we will make a listening socket
+    // socket exists in a name space but has no address assigned to it
     
+    // so we need to specify an address with addr
+    // server creates a socket, binds a NAME to a socket
+
+    // setsockopt() - ingests the file descriptor fd - set (theres also get)
+    // options on sockets
+    
+    
+    // int bind(fd, const struct sockaddr *addr, socklen_t addrlen); 
     
 
     return 0;
