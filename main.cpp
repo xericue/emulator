6e527ceb94278ece0f00b5e1c3b1a18ed4c932cf
xericue <106630482+xericue@users.noreply.github.com>
2026-05-13 22:45:42 +0000

    are we smearious; first class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 
+class WALEntry {
+    public:
+        int gurt {};
+    
+};
+
 int main() {
+    /*
     std::cout << "yo welcome to the write-heavy database bruh. you got a few commands:\n";
     std::cout << "set <key> <value>\nget <key>\ndel <key>\nrange <start> <end>\nexit\n";
     std::cout << "\nexample of range:\ndb> set item:001 bruh\ndb> set item:002 bruhh\ndb> range item:001 item:002\nitem:001:    bruh\nitem:002:    bruhh\n";
+    */
+
+    // GURTTT
+    [[maybe_unused]] WALEntry bruh {67};
+    std::cout << bruh.gurt << "\n"; // its not +; its << in C++
+
     return 0;
 }
